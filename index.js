c1e7e96adcbc04020dc1e6c0ed2eb2710b1a2215
Rifki F. Akbar <47451911+kikibanner@users.noreply.github.com>
2020-11-05 11:34:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 let notes = [
     {
       id: 1,
-      content: "HTML is easy",
+      content: "HTML is easy!",
       date: "2019-05-30T17:30:31.098Z",
       important: true
     },
